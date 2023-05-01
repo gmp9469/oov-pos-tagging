c0f0cdb2699c625b18a05160badca4f035d702b0
gmp9469 <83371057+gmp9469@users.noreply.github.com>
2023-05-01 17:46:47 -0400

    Add files via upload

--- contextual_embedding.py
+++ contextual_embedding.py
@@ -10,23 +10,25 @@ credit:
 }
 
 """
+#MUST PIP INSTALL FLAIR
 
+import flair
 from flair.data import Corpus
 from flair.datasets import ColumnCorpus
 from flair.embeddings import WordEmbeddings, StackedEmbeddings, FlairEmbeddings
 from flair.models import SequenceTagger
 from flair.trainers import ModelTrainer
+from flair.data import Sentence
 
 def train_contextual():
     # SPECIFY PATH TO CORPUS
-    data_folder = 'path'#!!!!!!
+    train_file = './training.pos'
 
     # SPECIFY COLUMN FORMAT
-    columns = {0: 'text', 1: 'pos', 2: 'ner'}
+    columns = {0: 'text', 1: 'pos'}
 
-    tag_type = 'pos'
-    corpus = ColumnCorpus(data_folder, columns, tag_type=tag_type)
-    tag_dictionary = corpus.make_tag_dictionary(tag_type=tag_type)
+    corpus = ColumnCorpus('', columns, train_file=train_file)
+    tag_dictionary = corpus.make_tag_dictionary(tag_type='pos')
     embedding_types = [
         FlairEmbeddings('news-forward'),
         FlairEmbeddings('news-backward'),
@@ -36,12 +38,12 @@ def train_contextual():
     tagger = SequenceTagger(hidden_size=256,
                             embeddings=embeddings,
                             tag_dictionary=tag_dictionary,
-                            tag_type=tag_type)
+                            tag_type='pos')
     trainer = ModelTrainer(tagger, corpus)
 
     trainer.train('./',
                   train_with_dev=True,
-                  max_epochs=150)
+                  max_epochs=5)
     
     return tagger
 
@@ -71,3 +73,25 @@ result, _ = trainer.evaluate(test_corpus, mini_batch_size=32)
 print(result.detailed_results)
 
 """
+file = open('./testing.words', 'r')
+words = []
+sentences = []
+for line in file:
+    row = line.split()
+    if row:
+        words.append(row[0])  
+    else:
+        if words:
+            sentences.append(words)
+        words = []
+
+tagger = train_contextual()
+submission = open("submission2.pos", "w")
+for words in sentences:
+    sentence = Sentence(' '.join(words))
+    tagger.predict(sentence)
+    tags = [token.tag for token in sentence.tokens]
+    for i in range(len(words)):
+        line = words[i] + "\t" + tags[i] + '\n'
+        submission.write(line)
+    submission.write("\n")
